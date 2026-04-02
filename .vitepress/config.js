a2be9c26d58d1bcf35134fc6da8dd2e97f82f45f
Penta <me@penta.sh>
2026-04-02 17:23:54 +0100

    add pipeline

---
+++ .vitepress/config.js
@@ -0,0 +1,10 @@
+export default {
+  // site-level options
+  title: 'Afterlife',
+  description: 'Guides on Privacy, OSINT, OPSEC, FOSS and Piracy',
+  base: '/repo/',
+
+  themeConfig: {
+    // theme-level options
+  }
+}
